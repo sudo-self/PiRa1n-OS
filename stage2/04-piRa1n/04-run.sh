181def137a4fad18c5f7a406d978d65750f9ee29
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-05-27 14:43:11 +0200

    Update 04-run.sh

--- stage2/04-piRa1n/04-run.sh
+++ stage2/04-piRa1n/04-run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash -e
 on_chroot << EOF
-  cd /home/pi/
-  git clone --single-branch --branch beta https://github.com/raspberryenvoie/piRa1n/
-  cd piRa1n
-  ./install_piRa1n.sh
+  curl -k https://raw.githubusercontent.com/raspberryenvoie/piRa1n/beta/install_piRa1n.sh | sh
   chown -R pi:pi /home/pi/piRa1n/
   chmod -R 755 /home/pi/piRa1n/
 EOF
