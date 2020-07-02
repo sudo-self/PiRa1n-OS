106f106ddc94a791b7fd3c9bb6f7d621adaf9341
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-07-02 13:37:19 +0200

    Fix incorrect path for git clone

--- stage2/04-piRa1n/01-run.sh
+++ stage2/04-piRa1n/01-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 # Start of the installation of piRa1n
-git clone https://github.com/raspberryenvoie/piRa1n.git ${ROOTFS_DIR}/home/pi/
+git clone https://github.com/raspberryenvoie/piRa1n.git ${ROOTFS_DIR}/home/pi/piRa1n/
 wget https://assets.checkra.in/downloads/linux/cli/arm/dde0ee4255403a427636bb76e09e409487f8be128af4b7d89fac78548bd5b35a/checkra1n -O ${ROOTFS_DIR}/home/pi/piRa1n/piRa1n # Download Checkra1n
 chmod +x ${ROOTFS_DIR}/home/pi/piRa1n/piRa1n
 # The following commands will enable piRa1n at startup
@@ -23,7 +23,7 @@ chmod -R 755 ${ROOTFS_DIR}/home/pi/piRa1n/
   
 # Start of the installation of piRa1n-web
 sed -i 's/.*DirectoryIndex.*/        DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm/g' ${ROOTFS_DIR}/etc/apache2/mods-available/dir.conf
-git clone https://github.com/raspberryenvoie/piRa1n-web.git ${ROOTFS_DIR}/home/pi/
+git clone https://github.com/raspberryenvoie/piRa1n-web.git ${ROOTFS_DIR}/home/pi/piRa1n-web/
 cd ${ROOTFS_DIR}/home/pi/piRa1n-web/
 cp index.php options.php shutdown.php style.css stylesheet.css update.php update_status.php ${ROOTFS_DIR}/var/www/html/
 on_chroot << EOF
