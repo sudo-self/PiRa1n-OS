5a2e395e11de9b282e3497d61679f5c4946a8643
raspberryenvoie <raspberryenvoie@protonmail.com>
2020-10-09 22:27:16 +0200

    Update piRa1n and piRa1n-web

--- stage2/04-piRa1n/01-run.sh
+++ stage2/04-piRa1n/01-run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
+checkra1n_source='https://assets.checkra.in/downloads/linux/cli/arm/d751f4b245bd4071c571654607ca4058e9e7dc4a5fa30639024b6067eebf5c3b/checkra1n'
+
 # Compile libirecovery
 git clone https://github.com/libimobiledevice/libirecovery.git ${ROOTFS_DIR}/home/pi/libirecovery/
 on_chroot << EOF
@@ -10,37 +12,55 @@ make install
 ldconfig
 EOF
 rm -rf ${ROOTFS_DIR}/home/pi/libirecovery/
+
 # Start of the installation of piRa1n
 git clone https://github.com/raspberryenvoie/piRa1n.git ${ROOTFS_DIR}/home/pi/piRa1n/
-wget https://assets.checkra.in/downloads/linux/cli/arm/dde0ee4255403a427636bb76e09e409487f8be128af4b7d89fac78548bd5b35a/checkra1n -O ${ROOTFS_DIR}/home/pi/piRa1n/checkra1n # Download Checkra1n
+
+# Install checkra1n
+curl -Lko ${ROOTFS_DIR}/home/pi/piRa1n/checkra1n $checkra1n_source
 chmod +x ${ROOTFS_DIR}/home/pi/piRa1n/checkra1n
-# The following commands will enable piRa1n at startup
-echo '[Unit]
+on_chroot << EOF
+cd /home/pi/piRa1n/
+./checkra1n --version > checkra1n_version 2>&1
+EOF
+cd ${ROOTFS_DIR}/home/pi/piRa1n/
+sed -i -n -e 2p checkra1n_version
+# Remove '# '
+sed -i 's/# //g' checkra1n_version
+# Lower case
+sed -i 's/\(.*\)/\L\1/' checkra1n_version
+cd -
+
+# Enable piRa1n at startup
+cat << EOF > ${ROOTFS_DIR}/etc/systemd/system/piRa1n.service
+[Unit]
 Description=piRa1n
 After=multi-user.target
 
 [Service]
-ExecStart=/home/pi/piRa1n/piRa1n.sh
-StandardOutput=file:/var/log/piRa1n.log
-StandardError=file:/var/log/piRa1n.log
+ExecStart=/home/pi/piRa1n/startup.sh
 
 [Install]
-WantedBy=multi-user.target' > ${ROOTFS_DIR}/etc/systemd/system/piRa1n.service
+WantedBy=multi-user.target
+EOF
 chmod 644 ${ROOTFS_DIR}/etc/systemd/system/piRa1n.service
+systemctl enable piRa1n.service
 on_chroot << EOF
 systemctl enable piRa1n.service
-chown -R pi:pi /home/pi/piRa1n/
 EOF
-chmod -R 755 ${ROOTFS_DIR}/home/pi/piRa1n/
-# End of the installation of piRa1n
+
 # Start of the installation of piRa1n-web
-sed -i 's/.*DirectoryIndex.*/        DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm/g' ${ROOTFS_DIR}/etc/apache2/mods-available/dir.conf
-git clone https://github.com/raspberryenvoie/piRa1n-web.git ${ROOTFS_DIR}/home/pi/piRa1n-web/
-cd ${ROOTFS_DIR}/home/pi/piRa1n-web/
-cp index.php options.php shutdown.php style.css stylesheet.css update.php update_status.php exit_recovery_mode.php odysseyra1n.php ${ROOTFS_DIR}/var/www/html/
-on_chroot << EOF
-echo -e "\n# piRa1n-web\nwww-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n/config.sh\nwww-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n/shutdown.sh\nwww-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n-web/update.sh\nwww-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n/exit_recovery_mode.sh\nwww-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n/odysseyra1n.sh\nwww-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n-web/odysseyra1n_done.sh\n# End of piRa1n-web" | sudo EDITOR='tee -a' visudo
-chown -R pi:pi /home/pi/piRa1n-web/
+git clone https://github.com/raspberryenvoie/piRa1n-web.git  ${ROOTFS_DIR}/home/pi/piRa1n-web/
+rm -rf ${ROOTFS_DIR}/var/www/html/*
+cp -R ${ROOTFS_DIR}/home/pi/piRa1n-web/html/* ${ROOTFS_DIR}/var/www/html/
+# Add sueirs file
+cat << EOF > ${ROOTFS_DIR}/etc/sudoers.d/piRa1n-web
+# piRa1n-web
+www-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n/piRa1n
 EOF
-chmod -R 755 ${ROOTFS_DIR}/home/pi/piRa1n-web/
-# End of the installation of piRa1n-web
+
+# Fix file permissions
+on_chroot << EOF
+chown -R pi:pi /home/pi/piRa1n*/
+EOF
+chmod -R 755 ${ROOTFS_DIR}/home/pi/piRa1n*/
